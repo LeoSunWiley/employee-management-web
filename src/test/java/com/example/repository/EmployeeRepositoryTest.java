9870b3291ab33660a41ce0f54f1853eedb08c8c7
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-01-13 12:21:13 +0800

    test

--- src/test/java/com/example/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/repository/EmployeeRepositoryTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import java.math.BigDecimal;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
